f3d07311fbd6677ec471cc988fcb0c357b099422
horkenW <horkenwong@gmail.com>
2016-03-16 17:12:00 +0800

    new nestable

---
+++ js/quizjs2.js
@@ -0,0 +1,15 @@
+!function (window, document){
+	'user strict';
+
+	String.prototype.dot = function() {
+    return '.' + this;
+  };
+
+  var defaults={
+
+  }
+  var quizsBox= function (){
+  	this.w=$(document);
+  } 
+	 
+}(window, document)
