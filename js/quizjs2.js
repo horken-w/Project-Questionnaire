0eef9058b0c660cd3dfee9fb434766db0d125c3b
horkenW <horkenwong@gmail.com>
2016-03-17 16:57:59 +0800

    create Createnode function
    create Createnode function

--- js/quizjs2.js
+++ js/quizjs2.js
@@ -8,17 +8,11 @@
     return '#' + this;
   };
 
-  function addEvent(obj,evt,fn) {
+  function addEvent(obj,evt,fn, arg) {
     if (obj.addEventListener)
-      obj.addEventListener(evt,fn,false);
+      obj.addEventListener(evt,function(){fn(arg)},false);
     else if (obj.attachEvent)
-      obj.attachEvent('on'+evt,fn);
-  }
-
-  function bildingSelected(e){ //select target group
-      var _this=this;
-      $('.group').removeClass('active');
-      $(_this).addClass('active');
+      obj.attachEvent('on'+evt,function(){fn(arg)});
   }
 
   function quizsBox(element){
@@ -34,7 +28,12 @@
         addMultline: 'textarea',
         addRadiobox: 'radiobox',
         addCheckbox: 'checkbox',
-        addmultRadio: 'likertscale'
+        addmultRadio: 'likertscale',
+        listNodeName: 'ol',
+        itemNodeName: 'li',
+        rootClass: 'dd',
+        listClass: 'dd-list',
+        itemClass: 'dd-item',
       };
 
   quizsBox.prototype={
@@ -48,70 +47,81 @@
       if(this.el.find('.group').length !== 0){
         this.el.find('.group').first().addClass('active');
         this.el.find('.group').map(function(i, val){
-          addEvent(val, 'click', bildingSelected);
+          addEvent(val, 'click', _this.bildingSelected, this);
         })
       }
-      
-      
-      
     },
     creatItemListener:function(addbtn){
       var _this=this, active=$(addbtn);
+      function creatElement(el, classnames){
+        var node=document.createElement(el);
+        if(typeof classnames==='string') node.className+=classnames
+        return node;
+      }
+
+      var _ol=creatElement(items.listNodeName, items.listClass),
+          _li=creatElement(items.itemNodeName, items.itemClass);
+
       switch (addbtn){
         case items.addGroupbtn.hashtag():
-          addEvent(active[0], 'click', _this.groupCreat);
+          addEvent(active[0], 'click', _this.groupCreat, _this.bildingSelected);
           break;
         case items.addSingleline.hashtag():
-          addEvent(active[0], 'click', _this.singInputCreat);
+          addEvent(active[0], 'click', _this.singInputCreat, 
+            [_ol, _li]);
           break;
         case items.addMultline.hashtag():
-          addEvent(active[0], 'click', _this.multInputCreat);
+          addEvent(active[0], 'click', _this.multInputCreat, [_ol, _li]);
           break;
         case items.addRadiobox.hashtag():
-          addEvent(active[0], 'click', _this.radioBoxCreat);
+          addEvent(active[0], 'click', _this.radioBoxCreat, [_ol, _li]);
           break;
         case items.addCheckbox.hashtag():
-          addEvent(active[0], 'click', _this.checkBoxCreat);
+          addEvent(active[0], 'click', _this.checkBoxCreat, [_ol, _li]);
           break;
         case items.addmultRadio.hashtag():
-          addEvent(active[0], 'click', _this.multradioBoxCreat);
+          addEvent(active[0], 'click', _this.multradioBoxCreat, [_ol, _li]);
           break;
       }
       
     },
-    groupCreat:function(){
+    groupCreat:function(active){
       var _this=this, section=$('<section class="group" />') ;
       var wrapper=_this.wrap === undefined ? $(items.rootEl.hashtag()) : _this.wrap;
       section.append($('<h1 class="dd-handle" />').text('題組'));
-      addEvent(section[0], 'click', bildingSelected);
+      addEvent(section[0], 'click', active, section[0]);
       wrapper.append(section);
     },
     singInputCreat:function(active){
-      var _this=this;
+      var _this=document.getElementsByClassName('active');
+      var _ol=active[0], _li=active[1];
       
-      console.log(1);
     },
     multInputCreat:function(active){
-      var _this=this;
-      var wrapper=_this.wrap === undefined ? $(items.rootEl.hashtag()) : _this.wrap;
-      console.log(1);
+      var _this=document.getElementsByClassName('active');
+      var _ol=active[0], _li=active[1];
+      
     },
     radioBoxCreat:function(active){
-      var _this=this;
-      var wrapper=_this.wrap === undefined ? $(items.rootEl.hashtag()) : _this.wrap;
-      console.log(1);
+      var _this=document.getElementsByClassName('active');
+      var _ol=active[0], _li=active[1];
+      
     },
     checkBoxCreat:function(active){
-      var _this=this;
-      var wrapper=_this.wrap === undefined ? $(items.rootEl.hashtag()) : _this.wrap;
-      console.log(1);
+      var _this=document.getElementsByClassName('active');
+      var _ol=active[0], _li=active[1];
+      
     },
     multradioBoxCreat:function(active){
-      var _this=this;
-      var wrapper=_this.wrap === undefined ? $(items.rootEl.hashtag()) : _this.wrap;
-      console.log(1);
+      var _this=document.getElementsByClassName('active');
+      var _ol=active[0], _li=active[1];
+      
     },
-    
+    bildingSelected:function(e){
+      var _this=e;
+      $('.group').removeClass('active');
+      $(_this).addClass('active');
+    }
   }
 
 
